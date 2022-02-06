54a44868f39d914294f0980983ca38de52de4d46
Sadayoshi Tada <tadainfra@gmail.com>
2022-02-06 17:15:55 +0900

    更新

--- ch01/chat/main.go
+++ ch01/chat/main.go
@@ -1,12 +1,16 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
+	"os"
+	"path/filepath"
 	"sync"
 	"text/template"
-	"path/filepath"
+	"github.com/tasogare0919/go-web-programing-develop/ch01/trace"
 )
+
 
 type templateHandler struct {
 	once sync.Once
@@ -23,21 +27,19 @@ func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+	var addr = flag.String("addr", ":8080", "The addr of the application.")
+	flag.Parse()
+
+	r := newRoom()
+	r.tracer = trace.New(os.Stdout)
+
 	http.Handle("/", &templateHandler{filename: "chat.html"})
-	// http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-	// 	w.Write([]byte(`
-	// 		<html>
-	// 			<head>
-	// 				<title>チャット</title>
-	// 			</head>
-	// 			<body>
-	// 				チャットしましょう
-	// 			</body>
-	// 		</html>
-	// 	`))
-	// })
-	// Webサーバーを始める
-	if err := http.ListenAndServe(":8080", nil); err != nil {
+	http.Handle("/room", r)
+
+	go r.run()
+
+	log.Println("Starting web serveron", *addr)
+	if err := http.ListenAndServe(*addr, nil); err != nil {
 		log.Fatal("ListenAndServe:", err)
 	}
 }
